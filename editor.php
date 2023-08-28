962a7f7dd5d1db49d3bd07b66a983ae71797b2dc
Quentin Orias <adressefictive@gmail.com>
2023-08-28 16:56:45 +0200

    editor layout

--- editor.php
+++ editor.php
@@ -4,22 +4,28 @@ require_once 'layout/header.php';
 ?>
 
 <main class="main_sidebar">
-<?php require_once __DIR__ . '/layout/sidebar.php' ?> 
+<?php require_once __DIR__ . '/layout/sidebar.php' ?>
 
-    <form action="edit.php" method="POST">
+    <div class="d-flex flex-column w-100">
 
-        <input class="form-control mb-3" type="email" name="email" id="email" placeholder="E-mail"/>
-        <input class="form-control mb-3" type="password" name="password" id="password" placeholder="Mot de passe"/>
+        <form class="edit_form text-center" action="edit.php" method="POST">
 
-        <?php if (isset($_GET['error'])) { ?>
-            <div class="error mb-3">
-                <?php echo ErrorCode::getErrorMessage(intval($_GET['error'])); ?>
-            </div>
-        <?php } ?>
+            <input class="form-control mb-3" type="text" name="title" id="title" placeholder="Titre de l'article"/>
+            <input class="form-control mb-3" type="text" name="categorie" id="categorie" placeholder="Catégorie"/>
+            <input class="form-control mb-3" type="file" name="image" id="image" placeholder="Image de l'article"/>
+            <textarea class="form-control mb-3" name="content" id="content" rows="6" placeholder="Contenu de l'article..."/></textarea>
 
-        <button type="submit">Connexion</button>
+            <?php if (isset($_GET['error'])) { ?>
+                <div class="error mb-3">
+                    <?php echo ErrorCode::getErrorMessage(intval($_GET['error'])); ?>
+                </div>
+            <?php } ?>
 
-    </form>
+            <button type="submit">Éditer</button>
+
+        </form>
+
+    </div>
 
 </main>
 
